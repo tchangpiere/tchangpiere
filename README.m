18e9905189a35507583ddac9d021da7096849e08
tchangpiere <143908445+tchangpiere@users.noreply.github.com>
2023-09-02 22:35:43 +0100

    Create README.m
    im hopping that some one can found a collaboration with this african boy

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @tchangpiere im from cameroon 14 year old
+- 👀 I’m interested in codage
+- 🌱 I’m currently learning about js html css and python
+- 💞️ I’m looking to collaborate with a amazing 
+- 📫 How to reach me ...
+
+<!---
+tchangpiere/tchangpiere is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
